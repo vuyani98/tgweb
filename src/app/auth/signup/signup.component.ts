567bccf9f2722b855ceed9c9a8de0b84d8dd7ea0
Vuyani Ncube <nvuyani307@gmail.com>
2022-11-04 15:33:36 +0200

    form validations

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { AuthService } from '../auth.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -17,7 +17,7 @@ export class SignupComponent implements OnInit {
     isReseller: false
   });
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService ) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router ) { }
 
   ngOnInit(): void {
   }
@@ -29,5 +29,6 @@ export class SignupComponent implements OnInit {
       return res
       })
     this.registerForm.reset()
+    this.router.navigateByUrl('/')
   }
 }
